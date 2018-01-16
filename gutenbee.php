d23df4b746019787fe01c5a24d14113bd8e8d5d5
anastis <anastis@4294cb29-2c39-44b5-8b85-408dc27d7d38>
2018-01-16 19:02:06 +0000

    Plugin header. Readme.
    git-svn-id: https://cssigniter.svn.beanstalkapp.com/cssigniter/plugins/gutenbee/trunk@20719 4294cb29-2c39-44b5-8b85-408dc27d7d38

--- gutenbee.php
+++ gutenbee.php
@@ -1,39 +1,46 @@
 <?php
-	/*
-	 * Plugin Name: GutenBee
-	 * Plugin URI: https://www.cssigniter.com/
-	 * Description: Premium Blocks for WordPress
-	 * Author: The CSSIgniter Team
-	 * Author URI: https://www.cssigniter.com
-	 * Version: 1.0.0
-	 */
+/*
+ * Plugin Name: GutenBee
+ * Plugin URI: https://www.cssigniter.com/
+ * Description: Premium Blocks for WordPress
+ * Author: The CSSIgniter Team
+ * Author URI: https://www.cssigniter.com/
+ * Version: 1.0.0
+ */
 
-	add_action( 'enqueue_block_editor_assets', 'gutenbee_enqueue_editor_assets' );
 
-	function gutenbee_enqueue_editor_assets() {
-		wp_enqueue_script(
-			'gutenbee',
+if ( ! defined( 'GUTENBEE_PLUGIN_VERSION' ) ) {
+	define( 'GUTENBEE_PLUGIN_VERSION', '1.0.0' );
+}
 
-			plugins_url( 'build/gutenbee.build.js', __FILE__ ),
-			array( 'wp-blocks', 'wp-element', 'wp-components', 'wp-utils', 'wp-i18n' ),
-			filemtime( plugin_dir_path( __FILE__ ) . 'build/gutenbee.build.js' )
-		);
+if ( ! defined( 'GUTENBEE_PLUGIN_DIR' ) ) {
+	define( 'GUTENBEE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
 
-		wp_enqueue_style(
-			'gutenbee-editor-css',
-			plugins_url( 'build/gutenbee.editor.css', __FILE__ ),
-			array( 'wp-edit-blocks' ),
-			filemtime( plugin_dir_path( __FILE__ ) . 'build/gutenbee.editor.css' )
-		);
-	}
+if ( ! defined( 'GUTENBEE_PLUGIN_DIR_URL' ) ) {
+	define( 'GUTENBEE_PLUGIN_DIR_URL', plugin_dir_url( __FILE__ ) );
+}
 
-	add_action( 'enqueue_block_assets', 'gutenbee_enqueue_block_assets' );
+add_action( 'enqueue_block_editor_assets', 'gutenbee_enqueue_editor_assets' );
 
-	function gutenbee_enqueue_block_assets() {
-		wp_enqueue_style(
-			'gutenbee-css',
-			plugins_url( 'build/gutenbee.style.css', __FILE__ ),
-			array( 'wp-blocks' ),
-			filemtime( plugin_dir_path( __FILE__ ) . 'build/gutenbee.style.css' )
-		);
-	}
+function gutenbee_enqueue_editor_assets() {
+	wp_enqueue_script( 'gutenbee', untrailingslashit( GUTENBEE_PLUGIN_DIR_URL ) . '/build/gutenbee.build.js', array(
+		'wp-blocks',
+		'wp-element',
+		'wp-components',
+		'wp-utils',
+		'wp-i18n',
+	), GUTENBEE_PLUGIN_VERSION );
+
+	wp_enqueue_style( 'gutenbee-editor', untrailingslashit( GUTENBEE_PLUGIN_DIR_URL ) . '/build/gutenbee.editor.css', array(
+		'wp-edit-blocks',
+	), GUTENBEE_PLUGIN_VERSION );
+}
+
+add_action( 'enqueue_block_assets', 'gutenbee_enqueue_block_assets' );
+
+function gutenbee_enqueue_block_assets() {
+	wp_enqueue_style( 'gutenbee', untrailingslashit( GUTENBEE_PLUGIN_DIR_URL ) . '/build/gutenbee.style.css', array(
+		'wp-blocks',
+	), GUTENBEE_PLUGIN_VERSION );
+}
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== GutenBee ===
+Contributors: cssigniterteam, silencerius
+Tags: gutenberg, blocks
+Requires at least: 4.9
+Tested up to: 4.9.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Requires PHP: 5.2
+
+GutenBee enhances the Gutenberg editor with more blocks!
+
+== Description ==
+GutenBee provides new blocks for the Gutenberg editor.
+
+**Blocks:**
+
+* Spacer
+* Divider
+
+With more on the way...
+
+== Installation ==
+1. Upload the plugin files to the `/wp-content/plugins/gutenbee` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the "Plugins" screen in WordPress
+3. New Gutenberg blocks will appear!
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release.
