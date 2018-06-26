14950b6e687f4f5150fbfb47604658a4a52b5b7b
vmasto <vmasto@4294cb29-2c39-44b5-8b85-408dc27d7d38>
2018-06-26 07:31:11 +0000

    Finish image comparison block
    git-svn-id: https://cssigniter.svn.beanstalkapp.com/cssigniter/plugins/gutenbee/trunk@22171 4294cb29-2c39-44b5-8b85-408dc27d7d38

---
+++ src/blocks/image-comparison/script.js
@@ -0,0 +1,13 @@
+import jQuery from 'jquery';
+import 'zurb-twentytwenty/js/jquery.event.move';
+import 'zurb-twentytwenty/js/jquery.twentytwenty';
+
+jQuery(($) => {
+  $('.wp-block-gutenbee-image-comparison').each(function () {
+    const $this = $(this);
+
+    $this.twentytwenty({
+      default_offset_pct: $this.data('offset') / 100,
+    });
+  });
+});
