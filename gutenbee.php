7f2b7f02178880f6952a2dd185469ce63b0f1dd3
anastis <anastis@4294cb29-2c39-44b5-8b85-408dc27d7d38>
2020-01-16 18:44:31 +0000

    - dev bump
    git-svn-id: https://cssigniter.svn.beanstalkapp.com/cssigniter/plugins/gutenbee/trunk@28258 4294cb29-2c39-44b5-8b85-408dc27d7d38

--- gutenbee.php
+++ gutenbee.php
@@ -25,7 +25,7 @@
 	 */
 
 	if ( ! defined( 'GUTENBEE_PLUGIN_VERSION' ) ) {
-		define( 'GUTENBEE_PLUGIN_VERSION', '2.1.0.1' );
+		define( 'GUTENBEE_PLUGIN_VERSION', '2.1.0.2' );
 	}
 
 	if ( ! defined( 'GUTENBEE_PLUGIN_DIR' ) ) {
