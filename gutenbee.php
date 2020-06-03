802ca1d2df0dc92c273d7234afd2f47bcbef219c
anastis <anastis@4294cb29-2c39-44b5-8b85-408dc27d7d38>
2020-06-03 14:18:31 +0000

    Changed version to 2.4.0 - Fixed version and stable tag information.
    git-svn-id: https://cssigniter.svn.beanstalkapp.com/cssigniter/plugins/gutenbee/trunk@29808 4294cb29-2c39-44b5-8b85-408dc27d7d38

--- gutenbee.php
+++ gutenbee.php
@@ -25,7 +25,7 @@
 	 */
 
 	if ( ! defined( 'GUTENBEE_PLUGIN_VERSION' ) ) {
-		define( 'GUTENBEE_PLUGIN_VERSION', '2.3.2' );
+		define( 'GUTENBEE_PLUGIN_VERSION', '2.4.0' );
 	}
 
 	if ( ! defined( 'GUTENBEE_PLUGIN_DIR' ) ) {
