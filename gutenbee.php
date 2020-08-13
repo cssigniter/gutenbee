7684809a397e3278c4f6733355db3e1d92cbb29c
nvourva <nvourva@4294cb29-2c39-44b5-8b85-408dc27d7d38>
2020-08-13 15:26:23 +0000

    updated version number
    git-svn-id: https://cssigniter.svn.beanstalkapp.com/cssigniter/plugins/gutenbee/trunk@30367 4294cb29-2c39-44b5-8b85-408dc27d7d38

--- gutenbee.php
+++ gutenbee.php
@@ -25,7 +25,7 @@
 	 */
 
 	if ( ! defined( 'GUTENBEE_PLUGIN_VERSION' ) ) {
-		define( 'GUTENBEE_PLUGIN_VERSION', '2.4.0' );
+		define( 'GUTENBEE_PLUGIN_VERSION', '2.5.0' );
 	}
 
 	if ( ! defined( 'GUTENBEE_PLUGIN_DIR' ) ) {
