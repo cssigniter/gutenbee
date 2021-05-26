2cebec217447842e9f81c37ff1f18ecdd370b3fd
anastis <anastis@4294cb29-2c39-44b5-8b85-408dc27d7d38>
2021-05-26 10:21:35 +0000

    - dev bump
    git-svn-id: https://cssigniter.svn.beanstalkapp.com/cssigniter/plugins/gutenbee/trunk@33347 4294cb29-2c39-44b5-8b85-408dc27d7d38

--- gutenbee.php
+++ gutenbee.php
@@ -5,7 +5,7 @@
  * Description: Premium Blocks for WordPress
  * Author: The CSSIgniter Team
  * Author URI: https://www.cssigniter.com
- * Version: 2.11.0.1
+ * Version: 2.11.0.2
  * Text Domain: gutenbee
  * Domain Path: languages
  *
