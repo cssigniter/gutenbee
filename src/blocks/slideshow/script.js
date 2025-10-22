0d15a7766790495b69d528b6dd7bec7d2d8253e5
Gerasimos Tsiamalos <tsiger@gmail.com>
2025-10-22 12:01:51 +0300

    FIXED: Slideshow would break on the frontend due to dep upgrade (Slick) in 2.19.0

--- src/blocks/slideshow/script.js
+++ src/blocks/slideshow/script.js
@@ -19,6 +19,7 @@ jQuery(window).on('load', () => {
       slidesToShow: $this.data('slides-to-show'),
       pauseOnHover: $this.data('pause-on-hover'),
       slide: '.gutenbee-slideshow-item',
+      rows: 0,
       prevArrow:
         '<button class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path d="M25.1 247.5l117.8-116c4.7-4.7 12.3-4.7 17 0l7.1 7.1c4.7 4.7 4.7 12.3 0 17L64.7 256l102.2 100.4c4.7 4.7 4.7 12.3 0 17l-7.1 7.1c-4.7 4.7-12.3 4.7-17 0L25 264.5c-4.6-4.7-4.6-12.3.1-17z"/></svg></button>',
       nextArrow:
